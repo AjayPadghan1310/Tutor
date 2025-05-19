b3d1bca437025bf314a34276e65aad8d81d39d8a
AjayPadghan <ajaypadghan1310@gmail.com>
2025-05-19 23:32:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -272,7 +272,7 @@ app.get('/tuter/:id', async (req, res) => {
 
 
 // Start the server
-const PORT = 8080 || process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
