a7b74017109a68aa0586a4eeae3175a572951613
AjayPadghan <ajaypadghan1310@gmail.com>
2025-05-19 21:56:07 +0530

    port change commit

--- app.js
+++ app.js
@@ -273,5 +273,6 @@ app.get('/tuter/:id', async (req, res) => {
 // Start the server
 const PORT = 8080;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
+
