be72adcc243d12a76d8606b5f0858034890fab95
AjayPadghan <ajaypadghan1310@gmail.com>
2025-05-19 21:51:57 +0530

    commiting new change

--- app.js
+++ app.js
@@ -272,6 +272,6 @@ app.get('/tuter/:id', async (req, res) => {
 
 // Start the server
 const PORT = 8080;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on http://0.0.0.0:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost${PORT}`);
 });
